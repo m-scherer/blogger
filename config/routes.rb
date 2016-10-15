ae31169662f61bb85681848f5ab00a3c2d9ae0b4
Michael Scherer <michael.c.scherer@gmail.com>
2016-10-15 16:12:52 -0600

    finished blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
+  resources :articles do
+    resources :comments
+  end
+
   root to: 'articles#index'
 
 end
